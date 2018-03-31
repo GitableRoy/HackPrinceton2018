f01952607a8fbd66fb4f84b1744bcfc39b02ad82
Mazlow Cohen <mcohen30@students.towson.edu>
2018-03-31 17:40:20 -0400

    update mongo

--- todo-api/app.js
+++ todo-api/app.js
@@ -9,6 +9,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// load mongoose package
+var mongoose = require('mongoose');
+// Use native Node promises
+mongoose.Promise = global.Promise;
+// connect to MongoDB
+mongoose.connect('mongodb://localhost/todo-api')
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
